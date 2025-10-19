e8cd8b4ef5e33177a6e13763eae9fd90bd6b2051
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-10-19 14:45:28 +0200

    we need to have implement the CRUD operations for user management using keycloak and its rest api. This way it is possible to just us the backend from the frontend without exposing the frontend and restrictions of the self-service in keycloak.

--- metric_definition.go
+++ metric_definition.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -11,14 +10,12 @@ import (
 
 func CreateMetricDefinition(pg *pgxpool.Pool) *Endpoint {
 	hl := HandlerFuncErr(func(w http.ResponseWriter, r *http.Request) error {
-		data, err := decode[metricv1.CreateMetricRequest](r)
+		_, err := decode[metricv1.CreateMetricRequest](r)
 		if err != nil {
 			return err
 		}
-		fmt.Println(data)
 		return encode[any](w, r, http.StatusCreated, nil)
 	})
-
 	return &Endpoint{
 		Handler: hl,
 	}
