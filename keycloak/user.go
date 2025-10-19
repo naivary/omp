466b8f0485c54c146c16dd01cb94b68a412f5ac4
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-10-19 16:34:38 +0200

    found thee nedpoints to create everything

--- keycloak/user.go
+++ keycloak/user.go
@@ -1,5 +1,11 @@
 package keycloak
 
-func CreateClubAdmin() error {
+type keycloak struct {
+	baseURL      string
+	clientSecret string
+}
+
+func (k keycloak) CreateClubRootUser() error {
+
 	return nil
 }
