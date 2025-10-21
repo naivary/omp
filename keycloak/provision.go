c16a3d801b8bc2b331278da99e15196b12ce601c
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-10-21 12:51:33 +0200

    keycloak provisioning is not done in the code

---
+++ keycloak/provision.go
@@ -0,0 +1,9 @@
+package keycloak
+
+import "context"
+
+func Provision(ctx context.Context, addr, adminClientID, adminClientSecret string) error {
+	// create the realms club and players
+	// create two clients
+	return nil
+}
