8c6c5c6506749d8c5ee4de61c68f379fb93707ea
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-10-21 13:05:29 +0200

    added a compose file for easier development transfer

--- keycloak/provision.go
+++
@@ -1,9 +0,0 @@
-package keycloak
-
-import "context"
-
-func Provision(ctx context.Context, addr, adminClientID, adminClientSecret string) error {
-	// create the realms club and players
-	// create two clients
-	return nil
-}
